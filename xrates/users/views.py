76e5af257e1f3aaf1b3ee6ed00d9c8c48b41e4d7
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-07-05 22:36:32 +0300

    feat: implement login and registration using Django built-ins

--- xrates/users/views.py
+++ xrates/users/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
-def ping(request):
-    return HttpResponse('pong')
+
+def login_view(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            login(request, login_form.get_user())
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': login_form})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            login(request, register_form.save())
+            return redirect('users:register')
+    else:
+        register_form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'form': register_form})
