e265803ced03dab0e73fd3090b78cc670f1dcdcf
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-07-07 00:04:08 +0300

    feat: add base template with navbar and styles

--- xrates/xrates/settings.py
+++ xrates/xrates/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'xrates.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'styles',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
