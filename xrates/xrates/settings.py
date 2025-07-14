c9da73b8b4838d5d6c36160a020e5ed4aed28f35
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-07-15 01:26:03 +0300

    fix: run collectstatic in Docker entrypoint

--- xrates/xrates/settings.py
+++ xrates/xrates/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'styles',
 ]
