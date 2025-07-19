3052bdfc9d8fbd275c15d555103a70f6171f92f4
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-07-19 20:21:11 +0300

    tests: add Unit tests for converter and rates

--- xrates/converter/tests.py
+++ xrates/converter/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from unittest.mock import MagicMock, patch
+from django.http import QueryDict
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from converter.views import converter_view
+from rates.tests import all_rates
+
+
+class ConverterTest(TestCase):
+    @patch('converter.views.ConverterHistory')
+    @patch('rates.views.cache.get')
+    @patch('converter.views.get_rates_from_api')
+    def test_converter(
+            self, get_rates, get_rates_from_cache, converts_history):
+        mocked_request = MagicMock()
+        mocked_request.method = 'POST'
+        mocked_request.POST = QueryDict('hryvnias_amount=100&currency=USD')
+        mocked_request.user = User()
+        converts_history.return_value = MagicMock()
+
+        get_rates.return_value = all_rates
+        get_rates_from_cache.return_value = None
+        
+        response = converter_view(mocked_request)
+
+        self.assertContains(response, '100 UAH = 2.4 USD')
--- xrates/rates/tests.py
+++ xrates/rates/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from rates.views import rates_view
+
+
+all_rates = [
+    {
+        "r030": 840,
+        "rate": 41.7514,
+        "cc": "USD",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 978,
+        "rate": 48.607,
+        "cc": "EUR",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 985,
+        "rate": 11.4397,
+        "cc": "PLN",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 826,
+        "rate": 56.1765,
+        "cc": "GBP",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 756,
+        "rate": 52.1241,
+        "cc": "CHF",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 392,
+        "rate": 0.28112,
+        "cc": "JPY",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 203,
+        "rate": 1.9743,
+        "cc": "CZK",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 9991,
+        "rate": 123.45,
+        "cc": "TEST1",
+        "exchangedate": "21.07.2025"
+    },
+    {
+        "r030": 9992,
+        "rate": 67.89,
+        "cc": "TEST2",
+        "exchangedate": "21.07.2025"
+    }
+]
+
+
+
+class RatesTest(TestCase):
+    @patch('rates.views.cache.get')
+    @patch('rates.views.get_rates_from_api')
+    def test_rates(self, get_rates, get_curr_from_cache,):
+        get_curr_from_cache.return_value = None
+        get_rates.return_value = all_rates
+        mocked_request = MagicMock()
+        response = rates_view(mocked_request)
+
+        self.assertContains(response, '<tr>', count=8)
