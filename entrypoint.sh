c7398270b57c8b0cebaba690e150368110319aa5
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2025-08-01 22:09:03 +0300

    chore: run app on uvicorn workers instead of basic gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cd xrates
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 xrates.wsgi:application
+python -m gunicorn --bind 0.0.0.0:8000 --workers 3 xrates.asgi:application -k uvicorn.workers.UvicornWorker --timeout 60
